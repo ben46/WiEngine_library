f3428d966211ccd72750b42b683101d2b09c4a27
ben zhou <ben02060846@gmail.com>
2012-06-08 22:31:31 +0800

    first repo

---
+++ jni/WiEngine/include/WiEngine-Box2D.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010 WiYun Inc.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __WiEngine_Box2D_h__
+#define __WiEngine_Box2D_h__
+
+/*
+ * Aggregated header file for WiEngine Box2D module. You must have
+ * libbox2d module included in your project
+ */
+
+#include "wyBox2D.h"
+#include "wyBox2DPELoader.h"
+#include "wyBox2DRender.h"
+#include "wyBox2DCollisionDetector.h"
+#include "wyFixtureAnimation.h"
+
+#endif // __WiEngine_Box2D_h__
---
+++ jni/WiEngine/include/types/wyColorHSV.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2010 WiYun Inc.
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __wyColorHSV_h__
+#define __wyColorHSV_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline wyColorHSV wychsv(float h, float s, float v) {
+	wyColorHSV c = { h, s, v };
+	return c;
+}
+
+/**
+ * \if English
+ * Convert hsv color to rgb color
+ *
+ * @param c hsv color
+ * @return rgb color
+ * \else
+ * 将hsv颜色转换为rgb颜色
+ *
+ * @param c hsv颜色
+ * @return rgb颜色
+ * \endif
+ */
+wyColor3B wychsv2c3b(wyColorHSV& c);
+
+/**
+ * \if English
+ * Convert hsv color to rgba color
+ *
+ * @param c hsv color
+ * @param alpha transparency value
+ * @return rgba color
+ * \else
+ * 将hsv颜色转换为rgba颜色
+ *
+ * @param c hsv颜色
+ * @param alpha 透明度
+ * @return rgba颜色
+ * \endif
+ */
+wyColor4B wychsv2c4b(wyColorHSV& c, unsigned char alpha);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __wyColorHSV_h__
